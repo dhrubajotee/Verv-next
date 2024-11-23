aedea9fd79c655705dc593333ef0400063c41f1a
dhrubajotee <djhowlader29@gmail.com>
2024-11-23 19:35:09 +0600

    update

---
+++ src/components/Pricing/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SimplifiedPricing from './SimplifiedPricing'
+
+const Pricing = () => {
+  return (
+    <div>
+        <SimplifiedPricing />
+    </div>
+  )
+}
+
+export default Pricing
